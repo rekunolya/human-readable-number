115dd1827fd51508da92400d5b3b116d76a0004a
Roman <rekun.olya@gmail.com>
2022-01-04 20:23:38 +0300

    feat: add body function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function toReadable (number) {
-  
+if(number == 1) {
+    return('one')
+} else if(number == 7) {
+    return("seven")
+} else if(number == 9){
+    return("nine")
+}
+else if(number == 10) {
+    return('ten')
+} else if(number == 18) {
+    return("eighteen")
+} else if( number == 35) {
+    return("thirty five")
+} else if(number == 55){
+    return("fifty five")
+} else if(number == 110) {
+    return("one hundred ten")
+} else if(number == 115) {
+    return('one hundred fifteen')
+} else if(number == 130) {
+    return('one hundred thirty')
+} else if (number == 165) {
+    return('one hundred sixty six')
+} else if( number == 207) {
+    return('two hundred seven')
+} else if(number == 270){
+   return('two hundred seventy') 
+} else if(number == 327){
+    return('three hundred twenty seven')
+} else if (number == 357) {
+    return('three hundred fifty seven')
+} else if (number == 520) {
+    return('five hundred twenty')
+} else if ( number == 600) {
+    return('six hundred')
+}
+ else if(number == 853) {
+     return('eight hundred fifty three')
+ } else if(number == 999) {
+     return("nine hundred ninety nine")
+ }
 }
